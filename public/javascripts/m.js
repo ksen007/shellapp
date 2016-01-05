8900475fba5e59062623fafc86efe4d5e43e549e
ksen <ksen@cs.berkeley.edu>
2016-01-04 22:03:10 -0800

    escape value in input

--- public/javascripts/m.js
+++ public/javascripts/m.js
@@ -1,5 +1,49 @@
 (function () {
     function C() {
+        var escapeMap = {
+            '&': '&amp;',
+            '<': '&lt;',
+            '>': '&gt;',
+            '"': '&quot;',
+            "'": '&#x27;',
+            '`': '&#x60;'
+        };
+
+        function invert(map) {
+            var ret = {};
+            for (var i in map) {
+                if (map.hasOwnProperty(i)) {
+                    ret[map[i]] = i;
+                }
+            }
+            return ret;
+        }
+
+        var unescapeMap = invert(escapeMap);
+
+        var createEscaper = function (map) {
+            var escaper = function (match) {
+                return map[match];
+            };
+
+            var keys = [];
+            for (var i in map) {
+                if (map.hasOwnProperty(i)) {
+                    keys.push(i);
+                }
+            }
+            var source = '(?:' + keys.join('|') + ')';
+            var testRegexp = RegExp(source);
+            var replaceRegexp = RegExp(source, 'g');
+            return function (string) {
+                string = string == null ? '' : '' + string;
+                return testRegexp.test(string) ? string.replace(replaceRegexp, escaper) : string;
+            };
+        };
+
+        this.escape = createEscaper(escapeMap);
+        this.unescape = createEscaper(unescapeMap);
+
         this.showView = function (name) {
             $('.m-view').each(function () {
                 if ($(this).attr('id') === 'm-' + name) {
